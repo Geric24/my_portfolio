a9c6fd7b2db4a72deafb9eeacf8e6a7d1cfabd03
Geric24 <gericdelarosa2@gmail.com>
2025-05-19 03:11:36 +0800

    asds

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
     'my-portfolio-bjq3.onrender.com',
+    'localhost',
+    '127.0.0.1', 
 ]
 
 
