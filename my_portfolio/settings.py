4efbccd43ad88475642d8fb3b84bf3237af58f56
Geric24 <gericdelarosa2@gmail.com>
2025-05-19 02:54:00 +0800

    added allowed host

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'my-portfolio-bjq3.onrender.com',
+]
 
 
 # Application definition
